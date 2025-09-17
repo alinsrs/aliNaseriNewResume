7f9a2cc467c34f4448aa8a07c4fdaceeeb011322
Ali Naseri <alinaseri797979@gmail.com>
2025-09-17 21:46:40 +0330

    1404/06/27 - 3

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ import Education from "./components/education.vue";
 <template>
   <div class="min-h-screen bg-background-1 dark:bg-background-dark-1 transition-all duration-200 ease-linear">
     <Header :info="info" class="sticky top-0 z-40"></Header>
-    <main class="mt-4 md:mt-8 md:px-4">
+    <main class="mt-8 px-4">
       <!-- Page content -->
       <div class="grid grid-cols-1 lg:grid-cols-4 gap-4 max-w-[1920px] w-[90%] mx-auto justify-center items-start">
         <aside class="bg-background-2 dark:bg-background-dark-2 text-text-1 dark:text-text-dark-1 col-span-2 xl:col-span-1
@@ -29,7 +29,7 @@ import Education from "./components/education.vue";
           <sidebar :info="info"></sidebar>
         </aside>
         <div
-            class="lg:col-span-2 xl:col-span-3 bg-background-2 text-text-1 dark:bg-background-dark-2 dark:text-text-dark-1 rounded-xl p-5
+            class="lg:col-span-2 xl:col-span-3 bg-background-2 text-text-1 dark:bg-background-dark-2 dark:text-text-dark-1 rounded-xl md:p-5
             transition-all duration-200 ease-linear flex flex-col gap-6">
           <aboutMe :info="info" id="about" class="scroll-mt-20"></aboutMe>
           <jobExperience :info="info" id="job" class="scroll-mt-20"></jobExperience>
