3c343e3233a4ad0953d6ceac9b724ac22c0ce11c
Ali Naseri <alinaseri797979@gmail.com>
2025-09-17 21:35:44 +0330

    1404/06/27

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ import Education from "./components/education.vue";
 <template>
   <div class="min-h-screen bg-background-1 dark:bg-background-dark-1 transition-all duration-200 ease-linear">
     <Header :info="info" class="sticky top-0 z-40"></Header>
-    <main class="mt-8 md:px-4">
+    <main class="mt-4 md:mt-8 md:px-4">
       <!-- Page content -->
       <div class="grid grid-cols-1 lg:grid-cols-4 gap-4 max-w-[1920px] w-[90%] mx-auto justify-center items-start">
         <aside class="bg-background-2 dark:bg-background-dark-2 text-text-1 dark:text-text-dark-1 col-span-2 xl:col-span-1
